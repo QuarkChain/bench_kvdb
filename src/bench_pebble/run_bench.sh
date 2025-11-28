9264790c4ef0387f913cd6eb923b9d2bcef8e199
ping-ke <molaokp@gmail.com>
2025-11-28 10:10:36 +0100

    add log

--- src/bench_pebble/run_bench.sh
+++ src/bench_pebble/run_bench.sh
@@ -16,6 +16,6 @@ sleep 10
 sleep 10
 ./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 512 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_512M.log
 sleep 10
-./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 51200 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_51200M.log
+./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 5120 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_5120M.log
 sleep 10
 ./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 51200 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_51200M.log
