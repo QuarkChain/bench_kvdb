404b18999321b72a33b60ce95d924189e7f6a662
ping-ke <molaokp@gmail.com>
2025-12-05 10:08:44 +0800

    update logs

--- src/bench_pebble/run_bench.sh
+++ src/bench_pebble/run_bench.sh
@@ -1,55 +1,60 @@
+./bench_pebble --T 200000000 --w 0 --l 2 --t 64 --c 23 --p ./data/bench_pebble_200m > logs/RadmonRead_200M_23M.log
+sleep 10
 ./bench_pebble --T 200000000 --w 0 --l 2 --t 64 --c 32 --p ./data/bench_pebble_200m > logs/RadmonRead_200M_32M.log
-sleep 10
-./bench_pebble --T 200000000 --w 0 --l 2 --t 64 --c 64 --p ./data/bench_pebble_200m > logs/RadmonRead_200M_64M.log
-sleep 10
-./bench_pebble --T 200000000 --w 0 --l 2 --t 64 --c 210 --p ./data/bench_pebble_200m > logs/RadmonRead_200M_210M.log
 sleep 10
 ./bench_pebble --T 200000000 --w 0 --l 2 --t 64 --c 45 --p ./data/bench_pebble_200m > logs/RadmonRead_200M_45M.log
 sleep 10
 ./bench_pebble --T 200000000 --w 0 --l 2 --t 64 --c 90 --p ./data/bench_pebble_200m > logs/RadmonRead_200M_90M.log
 sleep 10
-./bench_pebble --T 200000000 --w 0 --l 2 --t 64 --c 225 --p ./data/bench_pebble_200m > logs/RadmonRead_200M_225M.log
+./bench_pebble --T 200000000 --w 0 --l 2 --t 64 --c 135 --p ./data/bench_pebble_200m > logs/RadmonRead_200M_135M.log
 sleep 10
-./bench_pebble --T 200000000 --w 0 --l 2 --t 64 --c 338 --p ./data/bench_pebble_200m > logs/RadmonRead_200M_338M.log
+./bench_pebble --T 200000000 --w 0 --l 2 --t 64 --c 180 --p ./data/bench_pebble_200m > logs/RadmonRead_200M_180M.log
+sleep 10
+./bench_pebble --T 200000000 --w 0 --l 2 --t 64 --c 210 --p ./data/bench_pebble_200m > logs/RadmonRead_200M_210M.log
+sleep 10
+./bench_pebble --T 200000000 --w 0 --l 2 --t 64 --c 225 --p ./data/bench_pebble_200m > logs/RadmonRead_200M_225M.log
 sleep 10
 ./bench_pebble --T 200000000 --w 0 --l 2 --t 64 --c 450 --p ./data/bench_pebble_200m > logs/RadmonRead_200M_450M.log
 sleep 10
 ./bench_pebble --T 200000000 --w 0 --l 2 --t 64 --c 676 --p ./data/bench_pebble_200m > logs/RadmonRead_200M_676M.log
 sleep 10
 
+./bench_pebble --T 2000000000 --w 0 --l 2 --t 64 --c 226 --p ./data/bench_pebble_2b > logs/RadmonRead_2B_226M.log
+sleep 10
 ./bench_pebble --T 2000000000 --w 0 --l 2 --t 64 --c 284 --p ./data/bench_pebble_2b > logs/RadmonRead_2B_284M.log
-sleep 10
-./bench_pebble --T 2000000000 --w 0 --l 2 --t 64 --c 512 --p ./data/bench_pebble_2b > logs/RadmonRead_2B_512M.log
-sleep 10
-./bench_pebble --T 2000000000 --w 0 --l 2 --t 64 --c 2048 --p ./data/bench_pebble_2b > logs/RadmonRead_2B_2048M.log
 sleep 10
 ./bench_pebble --T 2000000000 --w 0 --l 2 --t 64 --c 452 --p ./data/bench_pebble_2b > logs/RadmonRead_2B_452M.log
 sleep 10
 ./bench_pebble --T 2000000000 --w 0 --l 2 --t 64 --c 905 --p ./data/bench_pebble_2b > logs/RadmonRead_2B_905M.log
 sleep 10
-./bench_pebble --T 2000000000 --w 0 --l 2 --t 64 --c 2263 --p ./data/bench_pebble_2b > logs/RadmonRead_2B_2263M.log
+./bench_pebble --T 2000000000 --w 0 --l 2 --t 64 --c 1360 --p ./data/bench_pebble_2b > logs/RadmonRead_2B_1360M.log
 sleep 10
-./bench_pebble --T 2000000000 --w 0 --l 2 --t 64 --c 3400 --p ./data/bench_pebble_2b > logs/RadmonRead_2B_3400M.log
+./bench_pebble --T 2000000000 --w 0 --l 2 --t 64 --c 1810 --p ./data/bench_pebble_2b > logs/RadmonRead_2B_1810M.log
+sleep 10
+./bench_pebble --T 2000000000 --w 0 --l 2 --t 64 --c 2048 --p ./data/bench_pebble_2b > logs/RadmonRead_2B_2048M.log
+sleep 10
+./bench_pebble --T 2000000000 --w 0 --l 2 --t 64 --c 2263 --p ./data/bench_pebble_2b > logs/RadmonRead_2B_2263M.log
 sleep 10
 ./bench_pebble --T 2000000000 --w 0 --l 2 --t 64 --c 4526 --p ./data/bench_pebble_2b > logs/RadmonRead_2B_4526M.log
 sleep 10
 ./bench_pebble --T 2000000000 --w 0 --l 2 --t 64 --c 6789 --p ./data/bench_pebble_2b > logs/RadmonRead_2B_6789M.log
 sleep 10
 
-
+./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 2250 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_2250M.log
+sleep 10
 ./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 2600 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_2600M.log
-sleep 10
-./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 5120 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_5120M.log
-sleep 10
-./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 21000 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_21000M.log
 sleep 10
 ./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 4500 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_4500M.log
 sleep 10
 ./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 9000 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_9000M.log
 sleep 10
-./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 22500 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_22500M.log
+./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 13500 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_13500M.log
 sleep 10
-./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 33750 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_33750M.log
+./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 18000 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_18000M.log
+sleep 10
+./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 21000 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_21000M.log
+sleep 10
+./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 22500 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_22500M.log
 sleep 10
 ./bench_pebble --T 20000000000 --w 0 --l 2 --t 64 --c 45000 --p ./data/bench_pebble_20b > logs/RadmonRead_20B_45000M.log
 sleep 10
